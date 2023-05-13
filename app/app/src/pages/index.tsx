9588e0342eb2e93565a446f8488bbc63f1f68122
macbook_kai <kokuyo2110@gmail.com>
2023-05-14 07:14:45 +0900

    16

--- app/app/src/pages/index.tsx
+++ app/app/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
-import {Main} from './components/Main/view/main'
+import {Main} from '../components/Main/view/main'
 
 export default function Home() {
   return (
